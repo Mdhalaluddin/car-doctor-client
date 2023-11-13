8253af7d6fc0659444b900af7c57863081035175
Halal1122 <mdhealaluddin505@gmail.com>
2023-11-13 23:22:56 +0600

    search text and price sort added

--- src/Pages/Home/Servises/Services.jsx
+++ src/Pages/Home/Servises/Services.jsx
@@ -5,21 +5,33 @@ import ServiceCart from "./ServiceCart/ServiceCart";
 const Services = () => {
     const [asc, setAsc] = useState(true)
     const [service, setService] = useState([]);
-
+    const [search, setSearch] = useState('')
+    console.log(search);
     useEffect(() => {
-        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'desc'}`)
+        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'des'}&search=${search}`)
             .then(res => res.json())
             .then(data => setService(data))
-    }, [asc])
+    }, [asc, search])
+
+    const handleSearch = e => {
+        e.preventDefault();
+        const searchText = e.target.search.value;
+        console.log(searchText);
+        setSearch(searchText)
+    }
     return (
         <div>
             <div className="text-center space-y-5">
                 <h3 className="text-2xl font-bold text-rose-700">Service</h3>
                 <h2 className="text-5xl font-bold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
-                <button onClick={()=>setAsc(!asc)} className="btn btn-secondary">
-                    {asc? 'Price: High to Low' : 'Price: Low to High'}
+                <button onClick={() => setAsc(!asc)} className="btn btn-secondary">
+                    {asc ? 'Price: Low to High' : 'Price: High to Low'}
                 </button>
+                <form onSubmit={handleSearch}>
+                    <input type="text" name="search" placeholder="search" />
+                    <input type="submit" className="btn btn-primary" value="Search" />
+                </form>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
